f07670de634faea1cef5d4492a7de52311a892f4
Jan Hendrych <st64124@upce.cz>
2022-12-08 14:44:21 +0100

    komentare

--- app/src/main/java/com/example/clickergame/view/GameActivity.kt
+++ app/src/main/java/com/example/clickergame/view/GameActivity.kt
@@ -20,20 +20,24 @@ class GameActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
 
+        //viewModel
         val factory = GameFactory()
         game = ViewModelProvider(this, factory).get(Game::class.java)
 
+        //Prvni prisera
         var actualHealth = findViewById<TextView>(R.id.actualHealthTxt)
         var maxHealth = findViewById<TextView>(R.id.maxHealthTxt)
         actualHealth.text = game.activeMonster.actualHealth.toString()
         maxHealth.text = game.activeMonster.maxHealth.toString()
 
+        //Prisera obrazek
         var monsterIMG = findViewById<ImageView>(R.id.monsterImage)
         monsterIMG.setImageResource(R.drawable.ic_launcher_background)
 
         var monsterHealth = findViewById<ProgressBar>(R.id.monsterHealth)
         monsterHealth.max = game.activeMonster.maxHealth
 
+        //Kliknuti na priseru
         monsterIMG.setOnClickListener(){
             game.gameClick()
             monsterHealth.setProgress(game.activeMonster.maxHealth - game.activeMonster.actualHealth,true)
